7bd82c0b177e97cee5cb376f2f066845339d67ce
harshiniram14 <727721euit053@skcet.ac.in>
2023-02-21 23:49:52 +0530

    first commit

---
+++ src/main/java/com/example/demo/classone.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class classone {
+	@RequestMapping("/hello")
+	public String display(HttpServletRequest req){
+		String name=req.getParameter("name");
+		HttpSession session = req.getSession();
+		session.setAttribute("displayName", name);
+		
+		return "NewFile.jsp";
+	}
+}
